72284de7880367727e9a253f4dd50f45b12d2218
EmireHaouas <emire.haouas100@gmail.com>
2025-03-11 23:01:11 +0100

    header is responsive on all devices & added darkmode

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,6 +5,8 @@ import verticalEllipsisIcon from '../../assets/Imgs/icon-vertical-ellipsis.svg';
 import addTaskMobile from '../../assets/Imgs/icon-add-task-mobile.svg';
 import iconDown from '../../assets/Imgs/icon-chevron-down.svg';
 import { useState } from 'react';
+import logoLight from '../../assets/Imgs/logo-light.svg';
+import logoDark from '../../assets/Imgs/logo-dark.svg';
 
 
 const Header = () => {
@@ -21,18 +23,26 @@ const Header = () => {
     return (    
         <>
         <header className={theme ? "headerLight" : "headerDark"}>
+            
             <div className="header_Pack1">
               <img className="logoMobile" src={logoMobile} alt="logo" />
-              <h1 className={theme ? 'task_CategoriesLight' : 'task_CategoriesDark'}>Plateform Lunch</h1>
+              <img className="logo_BigScreen" src={theme ?  logoDark : logoLight} alt="logo" />
+              <h1 className={theme ? 'task_CategoriesLight' : 'task_CategoriesDark'}>Plateform Launch</h1>
               <img className="iconDown" src={iconDown} alt="arrow down icon" />
             </div>
+
             <div className="header_Pack2">
-            
+              
+              <div className="addTask">
               <img className="addTaskMobile" src={addTaskMobile} alt="add task" />
+              <p className="p_AddTask">Add New Task</p>
+            </div>
             
             <img className="verticalIcon" src={verticalEllipsisIcon} alt="vertical ellipsis" />
             </div>
+        
         </header>
+        
         <div>
             <button onClick={changeTheme}>{theme ? 'light mode' : 'dark mode'}</button>
         </div>
